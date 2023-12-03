aff8e7dced12fc2bacae7ef2e2936c31aab94f7c
cvonszcz <152619844+cvonszcz@users.noreply.github.com>
2023-12-03 20:50:15 +0000

    complete re-write
    made it a training plan website
    added new image assets to replace test images
    added "to-do" page to test multiple webpagess

--- scripts/main.js
+++ scripts/main.js
@@ -2,32 +2,32 @@ const myImage = document.querySelector("img");
 
 myImage.onclick = () => {
   const mySrc = myImage.getAttribute("src");
-  if (mySrc === "images/test-image.png") {
-    myImage.setAttribute("src", "images/test-image2.png");
+  if (mySrc === "images/runnin-people.png") {
+    myImage.setAttribute("src", "images/a-c-hm.png");
   } else {
-    myImage.setAttribute("src", "images/test-image.png");
+    myImage.setAttribute("src", "images/runnin-people.png");
   }
 };
 let myButton = document.querySelector("button");
 let myHeading = document.querySelector("h1");
 
 function setUserName() {
-    const myName = prompt("Please enter your name.");
-    if (!myName) {
-      setUserName();
-    } else {
-      localStorage.setItem("name", myName);
-      myHeading.textContent = `thanks for visiting, ${myName}`;
-    }
-  }
-  
-
-  if (!localStorage.getItem("name")) {
+  const myName = prompt("Please enter your name.");
+  if (!myName) {
     setUserName();
   } else {
-    const storedName = localStorage.getItem("name");
-    myHeading.textContent = `sorry, refreshing doesn't do anything yet, ${storedName}`;
+    localStorage.setItem("name", myName);
+    myHeading.textContent = `You need to get off your ass and train, ${myName}!!`;
   }
-  myButton.onclick = () => {
-    setUserName();
-  };
+}
+
+
+if (!localStorage.getItem("name")) {
+  setUserName();
+} else {
+  const storedName = localStorage.getItem("name");
+  myHeading.textContent = `Refreshing ain't gonna do shit, ${storedName}, get out there and run!`;
+}
+myButton.onclick = () => {
+  setUserName();
+};
